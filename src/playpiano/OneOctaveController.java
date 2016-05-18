bdfaf34a12d16c24304b338c8525b5c4630916b8
pascale <pascale@wireless-s0-so-150-36-125.uchicago.edu>
2016-05-18 13:44:16 -0500

    Commit with OneOctave methods for middle keys

--- src/playpiano/OneOctaveController.java
+++ src/playpiano/OneOctaveController.java
@@ -7,7 +7,9 @@ package playpiano;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.media.AudioClip;
 
 /**
  * FXML Controller class
@@ -23,5 +25,77 @@ public class OneOctaveController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    public void playmC(){
+        AudioClip mC = new AudioClip(getClass().getResource("mC.wav").toString());
+        mC.play();
+    }
+    
+    @FXML
+    public void playmCsharp(){
+        AudioClip mCsharp = new AudioClip(getClass().getResource("mC#.wav").toString());
+        mCsharp.play();
+    }
+    
+    @FXML
+    public void playmD(){
+        AudioClip mD = new AudioClip(getClass().getResource("mD.wav").toString());
+        mD.play();
+    }
+    
+    @FXML
+    public void playmDsharp(){
+        AudioClip mDsharp = new AudioClip(getClass().getResource("mD#.wav").toString());
+        mDsharp.play();
+    }
+    
+    @FXML
+    public void playmE(){
+        AudioClip mE = new AudioClip(getClass().getResource("mE.wav").toString());
+        mE.play();
+    }
+    
+    @FXML
+    public void playmF(){
+        AudioClip mF = new AudioClip(getClass().getResource("mF.wav").toString());
+        mF.play();
+    }
+    
+    @FXML
+    public void playmFSharp(){
+        AudioClip mFSharp = new AudioClip(getClass().getResource("mFSharp.wav").toString());
+        mFSharp.play();
+    }
+    
+    @FXML
+    public void playmG(){
+        AudioClip mG = new AudioClip(getClass().getResource("mG.wav").toString());
+        mG.play();
+    }
+    
+    @FXML
+    public void playmGSharp(){
+        AudioClip mGSharp = new AudioClip(getClass().getResource("mGSharp.wav").toString());
+        mGSharp.play();
+    }
+    
+    @FXML
+    public void playmA(){
+        AudioClip mA = new AudioClip(getClass().getResource("mA.wav").toString());
+        mA.play();
+    }
+    
+    @FXML
+    public void playmAsharp(){
+        AudioClip mAsharp = new AudioClip(getClass().getResource("mASharp.wav").toString());
+        mAsharp.play();
+    }
+    
+    @FXML
+    public void playmB(){
+        AudioClip mB = new AudioClip(getClass().getResource("mB.wav").toString());
+        mB.play();
+    }
     
 }
